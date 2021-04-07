3098cf6dd8a936f94fb7638cc39e11f44932f69b
durdenliu <durdenliu@outlook.com>
2021-04-07 15:21:34 +0800

    增加了小球半径、小球颜色和暂停按钮

--- app/src/main/java/com/example/themovingball/MainActivity.java
+++ app/src/main/java/com/example/themovingball/MainActivity.java
@@ -1,32 +1,161 @@
 package com.example.themovingball;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.text.InputType;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
+import android.view.animation.LayoutAnimationController;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.PathInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HandshakeCompletedListener;
+
 public class MainActivity extends AppCompatActivity {
+    private MovingBallView mViewBall;
+    private int screenWidth;
+    private int screenHeight;
+    private Button mButtonRadius;
+    private Button mButtonColor;
+    private Button mButtonPause;
+
+    private enum Direction{
+        RIGHT (0),
+        DOWN (1);
+        Direction(int ni) {
+            nativeInt = ni;
+        }
+        final int nativeInt;
+    }
+    private Direction direction;
+    private int choice;
+
+    private ObjectAnimator animator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        mViewBall = findViewById(R.id.view_ball);
+
+        animator = new ObjectAnimator();
+
+        mButtonRadius = findViewById(R.id.button_radius);
+        mButtonRadius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText editText = new EditText(MainActivity.this);
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this).setTitle("半径：").setView(editText).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(mViewBall.getLayoutParams());
+                        lp.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, Integer.valueOf(editText.getText().toString()), getApplicationContext().getResources().getDisplayMetrics());
+                        lp.width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, Integer.valueOf(editText.getText().toString()), getApplicationContext().getResources().getDisplayMetrics());
+                        mViewBall.setLayoutParams(lp);
+                    }
+                });
+                dialog.show();
+
+            }
+        });
+
+        mButtonColor = findViewById(R.id.button_color);
+        mButtonColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String[] items = {"红色", "黄色", "白色", "黑色", "青色"};
+                choice = -1;
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this).setTitle("颜色")
+                        .setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                choice = which;
+                            }
+                        }).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (choice != -1) {
+                                    switch (items[choice].toString()) {
+                                        case "红色": {
+                                            mViewBall.setColor(Color.RED);
+                                            mViewBall.invalidate();
+                                            break;
+                                        }
+                                        case "黄色": {
+                                            mViewBall.setColor(Color.YELLOW);
+                                            mViewBall.invalidate();
+                                            break;
+                                        }
+                                        case "白色": {
+                                            mViewBall.setColor(Color.WHITE);
+                                            mViewBall.invalidate();
+                                            break;
+                                        }
+                                        case "黑色": {
+                                            mViewBall.setColor(Color.BLACK);
+                                            mViewBall.invalidate();
+                                            break;
+                                        }
+                                        case "青色": {
+                                            mViewBall.setColor(Color.CYAN);
+                                            mViewBall.invalidate();
+                                            break;
+                                        }
+                                        default:
+                                            break;
+                                    }
+                                }
+                            }
+                        });
+                dialog.create().show();
+            }
+        });
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,80 +166,176 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
         DisplayMetrics dm = getResources().getDisplayMetrics();
-        int screenWidth = dm.widthPixels;
-        int screenHeight = dm.heightPixels;
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
         int menuItemThatWasSelected = item.getItemId();
-        MovingBallView viewBall = (MovingBallView)findViewById(R.id.view_ball);
+        mViewBall = (MovingBallView)findViewById(R.id.view_ball);
+
         switch (menuItemThatWasSelected){
             case R.id.action_line_move:{
-                Context context = MainActivity.this;
-                String message = "直线运动!";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-                run_line(screenWidth, screenHeight / 2);
+
+                final String[] items = {"横向", "纵向"};
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("请选择运动方向");
+                dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        choice = which;
+                    }
+                });
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, items[choice] + "运动！", Toast.LENGTH_LONG).show();
+                        if(choice == 1){
+                            runLine(Direction.DOWN);
+                        } else {
+                            runLine(Direction.RIGHT);
+                        }
+                    }
+                });
+                dialog.show();
                 break;
             }
+
             case R.id.action_circle_move:{
-                Context context = MainActivity.this;
-                String message = "圆周运动!";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-                run_circle(screenWidth / 2, screenHeight / 2, screenWidth / 4);
+
+                final EditText editText = new EditText(this);
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this).setTitle("输入运行半径").setView(editText)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                final int radius;
+                                if(editText.getText().toString().length() > 0) {
+                                    radius = (int) Integer.valueOf(editText.getText().toString());
+                                } else {
+                                    radius = screenWidth / 4;
+                                }
+                                Context context = MainActivity.this;
+                                String message = "圆周运动!";
+                                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                                runCircle(screenWidth / 2, screenHeight / 2, radius);
+                            }
+                        });
+                dialog.create().show();
                 break;
             }
             case R.id.action_simple_harmonic:{
+
                 Context context = MainActivity.this;
                 String message = "简谐运动!";
                 Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-                run_SHM(screenWidth, screenWidth / 4, 5);
+                runSHM(screenWidth, screenWidth / 3, 2);
                 break;
             }
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void run_line(int toX, int toY){
-        MovingBallView viewBall = (MovingBallView)findViewById(R.id.view_ball);
+    private void runLine(Direction direction){
+        mViewBall = (MovingBallView)findViewById(R.id.view_ball);
 
         Path path = new Path();
-        path.lineTo(toX, toY);
+        path.setLastPoint(0, 0);
+        if (direction == Direction.DOWN){
+            path.rLineTo(0, screenHeight);
+        } else if (direction == Direction.RIGHT){
+            path.rLineTo(screenWidth, 0);
+        }
 
-        ObjectAnimator animator = ObjectAnimator.ofFloat(viewBall, View.X, View.Y, path);
+        animator.cancel();
+        animator = ObjectAnimator.ofFloat(mViewBall, mViewBall.X, mViewBall.Y, path);
         animator.setDuration(2000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.start();
+
+        mButtonPause = findViewById(R.id.button_pause);
+        ObjectAnimator finalAnimator = animator;
+        mButtonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonPause.getText().toString() == "暂停"){
+                    finalAnimator.pause();
+                    mButtonPause.setText("继续");
+                } else {
+                    finalAnimator.resume();
+                    mButtonPause.setText("暂停");
+                }
+
+            }
+        });
     }
 
-    private void run_circle(int centerX, int centerY, int radius){
-        MovingBallView viewBall = (MovingBallView)findViewById(R.id.view_ball);
+    private void runCircle(int centerX, int centerY, int radius){
+        mViewBall = (MovingBallView)findViewById(R.id.view_ball);
+        mViewBall.animate().cancel();
 
         Path path = new Path();
         path.addCircle(centerX, centerY, radius, Path.Direction.CCW);
 
-        ObjectAnimator animator = ObjectAnimator.ofFloat(viewBall, View.X, View.Y, path);
+        animator = ObjectAnimator.ofFloat(mViewBall, mViewBall.X, mViewBall.Y, path);
         animator.setDuration(2000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.start();
+
+        mButtonPause = findViewById(R.id.button_pause);
+        ObjectAnimator finalAnimator = animator;
+        mButtonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonPause.getText().toString() == "暂停"){
+                    finalAnimator.pause();
+                    mButtonPause.setText("继续");
+                } else {
+                    finalAnimator.resume();
+                    mButtonPause.setText("暂停");
+                }
+
+            }
+        });
+
     }
 
-    private void run_SHM (int amplitude, int wavelength, int period){
+    private void runSHM (int amplitude, int wavelength, int period){
 
-        int waveControl = 60;
+        mViewBall = (MovingBallView)findViewById(R.id.view_ball);
 
-        MovingBallView viewBall = (MovingBallView)findViewById(R.id.view_ball);
 
         Path path = new Path();
-
-        for (int i = -wavelength; i <  period * wavelength; i += wavelength){
+        path.setLastPoint(0, 0);
+        for (int i = -wavelength; i <  period * wavelength; i += wavelength) {
 
             path.rQuadTo(wavelength / 4, amplitude, wavelength / 2, 0);
             path.rQuadTo(wavelength / 4, amplitude, wavelength / 2, 0);
         }
 
-        path.close();
-
-        ObjectAnimator animator = ObjectAnimator.ofFloat(viewBall, View.X, View.Y, path);
+        animator = ObjectAnimator.ofFloat(mViewBall, mViewBall.X, mViewBall.Y, path);
         animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(10000);
+        animator.setDuration(5000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.start();
+
+        mButtonPause = findViewById(R.id.button_pause);
+        ObjectAnimator finalAnimator = animator;
+        mButtonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonPause.getText().toString() == "暂停"){
+                    finalAnimator.pause();
+                    mButtonPause.setText("继续");
+                } else {
+                    finalAnimator.resume();
+                    mButtonPause.setText("暂停");
+                }
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/themovingball/MovingBallView.java
+++ app/src/main/java/com/example/themovingball/MovingBallView.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.constraintlayout.widget.Constraints;
 
 
 public class MovingBallView extends View {
 
+    private int mColor = Color.BLACK;
 
     public MovingBallView(Context context){
         super(context);
@@ -19,22 +25,32 @@ public class MovingBallView extends View {
         super(context, attrs);
     }
 
+    public MovingBallView(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+    }
+
+
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
-        int radius = getMeasuredWidth() / 2;
 
-        int ballX = getLeft() + radius;
-
-        int ballY = getTop() + radius;
+        int mRadius = getWidth() / 2;
+        int ballX = mRadius;
+        int ballY = mRadius;
 
         Paint paint = new Paint();
-        paint.setColor(Color.WHITE);
 
-        canvas.drawCircle(ballX, ballY, radius, paint);
+        paint.setColor(mColor);
+        canvas.drawCircle(ballX, ballY, mRadius, paint);
+    }
+
+    public void setColor(int color){
+        mColor = color;
 
     }
+
+
 
 }
 
